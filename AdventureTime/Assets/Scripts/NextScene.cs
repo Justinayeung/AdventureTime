31c4f1383a6c6767bc26daea31f72c0166d5f8e5
Nicole <polidorenn@gmail.com>
2020-02-16 15:36:26 -0500

    Next Scene Script

---
+++ AdventureTime/Assets/Scripts/NextScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextScene : MonoBehaviour
+{
+
+    public string SceneName;
+
+   void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneName);
+        }
+    }
+}
