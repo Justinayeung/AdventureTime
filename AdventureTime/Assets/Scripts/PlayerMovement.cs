6f0c29b482971c7e1239b6e763688d67eef8c8b1
Justinayeung <yeunj511@newschool.edu>
2020-02-16 16:23:23 -0500

    Player movement script and imported example/test platform models

--- AdventureTime/Assets/Scripts/PlayerMovement.cs
+++ AdventureTime/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float speed;
+
+    Rigidbody rb;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        PlayerMove();
+    }
+
+    /// <summary>
+    /// Move character using unity input (WASD or Arrows)
+    /// </summary>
+    public void PlayerMove() {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.velocity = (movement * speed);
     }
 }
