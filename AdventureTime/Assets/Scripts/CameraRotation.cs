00e7303c667a096b2d4069dce089fb6a1a2cc194
Nicole <polidorenn@gmail.com>
2020-02-16 18:29:48 -0500

    camera rotation

---
+++ AdventureTime/Assets/Scripts/CameraRotation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+
+    public float speed;
+
+    void Update()
+    {
+        transform.Rotate(0, speed * Time.deltaTime, 0);
+
+
+    }
+}
