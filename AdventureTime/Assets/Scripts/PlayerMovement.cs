ce5d3779716a83a416dbb91bbabdc42d27411749
Justinayeung <yeunj511@newschool.edu>
2020-02-20 14:23:53 -0500

    Probuilder

--- AdventureTime/Assets/Scripts/PlayerMovement.cs
+++ AdventureTime/Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.AI;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -24,7 +25,14 @@ public class PlayerMovement : MonoBehaviour
     public void PlayerMove() {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        rb.velocity = (movement * speed);
+        Vector3 movement = new Vector3(moveHorizontal * speed, rb.velocity.y, moveVertical * speed);
+        rb.velocity = movement;
+    }
+
+    /// <summary>
+    /// Touch to move player
+    /// </summary>
+    public void TouchController() {
+
     }
 }
