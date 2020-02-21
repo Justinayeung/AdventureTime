ca2992a3ef81c39941dfe6437f4a34af93a641dc
Nicole <polidorenn@gmail.com>
2020-02-21 11:49:05 -0500

    riddle appearing on plaque when colliding into collider

---
+++ AdventureTime/Assets/Scripts/Plaque.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Plaque : MonoBehaviour
+{
+    public bool isRiddleThere = false;
+    public GameObject Riddle;
+
+    public Text riddleText;
+    public Animator riddleAnim;
+
+    private void Start()
+    {
+        Riddle.SetActive(false);
+
+    }
+
+    private void Update()
+    {
+        //if (isRiddleThere)
+        //{
+        //    return;
+        //}
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            isRiddleThere = true;
+            Riddle.SetActive(true);
+            riddleAnim.SetBool("isShowing", true);
+
+        }
+    }
+
+    public void ExitRiddle()
+    {
+        Riddle.SetActive(false);
+        riddleAnim.SetBool("isShowing", false);
+
+    }
+}
