f558358313317a85b1dcdb9131d0673c20cdc7a0
Justinayeung <yeunj511@newschool.edu>
2020-02-22 16:51:50 -0500

    Moving between scenes, player movement (tap to move, mouse click to move)

--- AdventureTime/Assets/Scripts/PlayerMovement.cs
+++ AdventureTime/Assets/Scripts/PlayerMovement.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header ("References")]
+    public Camera cam;
+    public NavMeshAgent agent;
+
+    [Header ("Variables")]
     public float speed;
 
     Rigidbody rb;
+    Vector3 touchPos;
 
     // Start is called before the first frame update
     void Start() {
@@ -17,6 +23,8 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update() {
         PlayerMove();
+        TouchController();
+        MouseClickController();
     }
 
     /// <summary>
@@ -33,6 +41,34 @@ public class PlayerMovement : MonoBehaviour
     /// Touch to move player
     /// </summary>
     public void TouchController() {
+        for (int i = 0; i < Input.touchCount; i++) {
+            Touch touch = Input.GetTouch(i);
 
+            //If screen if tapped
+            if (touch.phase == TouchPhase.Ended && touch.tapCount == 1) {
+                touchPos = touch.position; //Setting touchPos to where screen was tapped
+                Ray ray = cam.ScreenPointToRay(touchPos); //Take touch position and convert it to a ray
+                RaycastHit hit; //Stores info of what the ray hits
+
+                //Move our NavMeshAgent
+                if (Physics.Raycast(ray, out hit)) { //Shoot out ray and checks if it hits something
+                    agent.SetDestination(hit.point);
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Mouse click to move player
+    /// </summary>
+    public void MouseClickController() {
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition); //Take mouse position and convert it to a ray
+        RaycastHit hit; //Stores info of what the ray hits
+
+        //Move our NavMeshAgent
+        if (Physics.Raycast(ray, out hit))
+        { //Shoot out ray and checks if it hits something
+            agent.SetDestination(hit.point);
+        }
     }
 }
