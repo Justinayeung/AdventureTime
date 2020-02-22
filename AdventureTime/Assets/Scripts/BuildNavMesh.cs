f89d8861cd84f023e51cf05d036ee6349647643d
Justinayeung <yeunj511@newschool.edu>
2020-02-22 17:15:57 -0500

    More environment things, organizing hierarchy

---
+++ AdventureTime/Assets/Scripts/BuildNavMesh.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BuildNavMesh : MonoBehaviour
+{
+    public NavMeshSurface surface;
+
+    public void Awake() {
+        //Build NavMesh on awake
+        surface.BuildNavMesh();
+    }
+}
