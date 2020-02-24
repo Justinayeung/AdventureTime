fa307787e9dcd4db93163551061a5a629c8e87da
Justinayeung <yeunj511@newschool.edu>
2020-02-24 14:12:32 -0500

    Plaque riddle

--- AdventureTime/Assets/Scripts/Plaque.cs
+++ AdventureTime/Assets/Scripts/Plaque.cs
@@ -5,41 +5,19 @@ using UnityEngine.UI;
 
 public class Plaque : MonoBehaviour
 {
-    public bool isRiddleThere = false;
-    public GameObject Riddle;
-
-    public Text riddleText;
     public Animator riddleAnim;
 
-    private void Start()
-    {
-        Riddle.SetActive(false);
-
-    }
-
-    private void Update()
-    {
-        if (isRiddleThere)
-        {
-            return;
-        }
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            isRiddleThere = true;
-            Riddle.SetActive(true);
+    public void OnTriggerStay(Collider other) {
+        if (other.gameObject.CompareTag("Player")){
             riddleAnim.SetBool("isShowing", true);
 
         }
     }
 
-    public void ExitRiddle()
-    {
-        Riddle.SetActive(false);
-        riddleAnim.SetBool("isShowing", false);
+    public void OnTriggerExit(Collider other) {
+        if (other.gameObject.CompareTag("Player")){
+            riddleAnim.SetBool("isShowing", false);
 
+        }
     }
 }
