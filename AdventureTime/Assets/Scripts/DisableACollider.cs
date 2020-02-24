53faed68d25838cd7e2519613640bcee8ae2aa05
Nicole <polidorenn@gmail.com>
2020-02-24 10:48:29 -0500

    sceneCollider main pathway

---
+++ AdventureTime/Assets/Scripts/DisableACollider.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableACollider : MonoBehaviour
+{
+
+    public static bool SceneChanged;
+    public GameObject beforeScene;
+    public GameObject afterScene;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(gameObject befo)
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
