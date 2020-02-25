f33a35805a0afcc3d718ba2292f31209c45d2910
songy696 <ysong.yjs@gmail.com>
2020-02-25 03:30:21 -0500

    pickup&inventory

---
+++ AdventureTime/Assets/Scripts/Inventory Scripts/PickUp.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    private Inventory inventory;
+    public GameObject itemThatPickedUp;
+    void Start()
+    {
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+    }
+    /*
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) {
+            for (int i = 0; i < inventory.slots.Length; i++)
+            {
+                if(inventory.isFull[i] == false) // this means inventory has more space to add items
+                {
+                    inventory.isFull[i] = true; // inventory is full
+                    Instantiate(itemThatPickedUp, inventory.slots[i].transform, false);
+                    Destroy(gameObject);
+                    break; // this break the for loop of checking empty slots
+                }
+            }
+        }
+    }
+    */
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            for (int i = 0; i < inventory.slots.Length; i++)
+            {
+                if (inventory.isFull[i] == false) // this means inventory has more space to add items
+                {
+                    inventory.isFull[i] = true; // inventory is full
+                    Instantiate(itemThatPickedUp, inventory.slots[i].transform, false);
+                    Destroy(gameObject);
+                    break; // this break the for loop of checking empty slots
+                }
+            }
+        }
+    }
+}
+
