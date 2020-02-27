61d1cbd293c04de297cb457f194e28b673af4762
Justinayeung <yeunj511@newschool.edu>
2020-02-27 13:23:51 -0500

    Image fixing so that dialogue can work

--- AdventureTime/Assets/Scripts/Scene Transitions/CallingLoadText.cs
+++ AdventureTime/Assets/Scripts/Scene Transitions/CallingLoadText.cs
@@ -1,19 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CallingLoadText : MonoBehaviour
 {
     private LoadingText loadingText;
+    private Image black;
 
     // Start is called before the first frame update
     void Start() {
         //Finding gameobject with tag LoadingText
+        black = GetComponent<Image>();
         loadingText = GameObject.FindGameObjectWithTag("LoadingText").GetComponent<LoadingText>();
     }
 
     //Setting loading text bool to false
     public void CallingText() {
         loadingText.loadScene = false;
+    }
+
+    public void EnableImage() {
+        black.enabled = true;
+    }
+
+    public void DisableImage() {
+        black.enabled = false;
     }
 }
