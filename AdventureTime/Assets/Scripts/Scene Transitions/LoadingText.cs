a45abf4eb91577ab3366ff5514c30c128bcf96fe
Justinayeung <yeunj511@newschool.edu>
2020-02-26 21:45:58 -0500

    Flower Field

---
+++ AdventureTime/Assets/Scripts/Scene Transitions/LoadingText.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadingText : MonoBehaviour
+{
+    public bool loadScene;
+    private Text loadingText;
+    
+    // Start is called before the first frame update
+    void Start() {
+        loadingText = GetComponent<Text>();
+        loadScene = true; //Start every scene with it true
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //If loadScene is false
+        if (!loadScene) {
+            loadingText.text = ""; //Setting text
+        }
+
+        //If loadScene is true
+        if (loadScene) {
+            loadingText.text = "LOADING. . ."; //Setting text
+            //Change the transparency of the text
+            loadingText.color = new Color(loadingText.color.r, loadingText.color.g, loadingText.color.b, Mathf.PingPong(Time.time, 1));
+        }
+    }
+}
---
+++ AdventureTime/Assets/Scripts/Scene Transitions/Positioning Scripts/PathToChestRidPosition.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathToChestRidPosition : SceneController
+{
+    public Transform player;
+    public Transform mainPath; //From MainPathway
+    public Transform flower; //From FlowerField
+    public Transform chest; //From ChestRiddle
+
+    //Checking which is the previous scene and setting location 
+    public override void Start(){
+        base.Start();
+        StartCoroutine(CharacterPosition());
+    }
+
+    IEnumerator CharacterPosition(){
+        yield return new WaitForSeconds(0.3f);
+        if (prevScene == "MainPathway") {
+            player.position = mainPath.position;
+        } else if (prevScene == "FlowerField") {
+            player.position = flower.position;
+        } else if (prevScene == "ChestRiddle") {
+            player.position = chest.position;
+        }
+    }
+}
