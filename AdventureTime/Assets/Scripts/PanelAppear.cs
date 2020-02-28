176187e3b7fefd3212ddf2b27987bbf447c48fb6
Nicole <polidorenn@gmail.com>
2020-02-28 15:06:56 -0500

    panel for end of game

---
+++ AdventureTime/Assets/Scripts/PanelAppear.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PanelAppear : MonoBehaviour
+{
+    public GameObject panel;
+    void Start()
+    {
+     GameObject panel =GameObject.GetComponent<>();
+
+      StartCoroutine(ExitPanel());
+    }
+
+    IEnumerator ExitPanel()
+    {
+
+       
+        yield return new WaitForSeconds(1f);
+        panel.SetActive(true);
+    }
+    
+}
