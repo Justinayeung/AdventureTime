f9dfd88f480358311231791b56843ef1c1b9f408
Nicole <polidorenn@gmail.com>
2020-03-02 16:32:12 -0500

    note code

---
+++ AdventureTime/Assets/Scripts/Note.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Note : MonoBehaviour
+{
+    public Image noteImage;
+
+    public AudioClip noteAudio;
+    public AudioClip notePutAway;
+    void Start()
+    {
+        //disabling the note to appear
+        noteImage.enabled = false;
+       
+    }
+
+    public void ShowNoteImage()
+    {
+        //function for what the note will do once player interacts with it
+        noteImage.enabled = true;
+        GetComponent<AudioSource>().PlayOneShot(noteAudio);
+    }
+
+    public void HideNote()
+    {
+        //function for what the note will do once put away
+        noteImage.enabled = false;
+        GetComponent<AudioSource>().PlayOneShot(notePutAway);
+    }
+}
