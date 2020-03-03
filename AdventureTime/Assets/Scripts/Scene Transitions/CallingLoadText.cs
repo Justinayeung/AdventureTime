18bec987cb36e001c665a373dd20db5225125315
Justinayeung <yeunj511@newschool.edu>
2020-03-03 13:00:03 -0500

    Scene transition update, adding bool to check if a canvas is up or not, started editing pathways

--- AdventureTime/Assets/Scripts/Scene Transitions/CallingLoadText.cs
+++ AdventureTime/Assets/Scripts/Scene Transitions/CallingLoadText.cs
@@ -5,19 +5,11 @@ using UnityEngine.UI;
 
 public class CallingLoadText : MonoBehaviour
 {
-    private LoadingText loadingText;
     private Image black;
 
     // Start is called before the first frame update
     void Start() {
-        //Finding gameobject with tag LoadingText
         black = GetComponent<Image>();
-        loadingText = GameObject.FindGameObjectWithTag("LoadingText").GetComponent<LoadingText>();
-    }
-
-    //Setting loading text bool to false
-    public void CallingText() {
-        loadingText.loadScene = false;
     }
 
     public void EnableImage() {
