58b258f4de4e0f699faf30ef514e5383eaa5ac69
Justinayeung <yeunj511@newschool.edu>
2020-03-05 14:52:31 -0500

    Chest riddle, and amaranthus animation

---
+++ AdventureTime/Assets/Scripts/AmaranthusObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmaranthusObject : MonoBehaviour
+{
+    public GameObject flower;
+    public Animator anim;
+
+    void Awake()
+    {
+        flower.SetActive(false);
+        anim.SetBool("CanSee", false);
+    }
+
+    public void setAmaranthusTrue() {
+        flower.SetActive(true);
+        anim.SetBool("CanSee", true);
+    }
+}
