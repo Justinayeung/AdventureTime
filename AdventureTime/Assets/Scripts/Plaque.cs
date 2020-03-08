0b80d1cfeb4776c78a3333d22e63ee93df8fea3f
Nicole <polidorenn@gmail.com>
2020-03-08 13:52:19 -0400

    plaque audio

--- AdventureTime/Assets/Scripts/Plaque.cs
+++ AdventureTime/Assets/Scripts/Plaque.cs
@@ -6,10 +6,13 @@ using UnityEngine.UI;
 public class Plaque : MonoBehaviour
 {
     public Animator riddleAnim;
-
+    public AudioSource plaqueAudio;
+    public AudioClip plaqueClip;
     public void OnTriggerEnter(Collider other) {
         if (other.gameObject.CompareTag("Player")){
             riddleAnim.SetBool("isShowing", true);
+            plaqueAudio.PlayOneShot(plaqueClip);
+
         }
     }
 }
