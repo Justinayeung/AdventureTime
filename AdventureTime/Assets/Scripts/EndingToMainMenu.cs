144f00fb6bb4dbe3d7b74a6771eb6b3a16cf9e6b
Justinayeung <yeunj511@newschool.edu>
2020-03-08 23:03:43 -0400

    last god dicussion and main menu

---
+++ AdventureTime/Assets/Scripts/EndingToMainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class EndingToMainMenu : MonoBehaviour
+{
+    public string sceneName;
+    public Image black;
+    public Animator anim;
+
+    void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player")) {
+            StartCoroutine(Ending()); //Starting fade coroutine
+        }
+    }
+
+    IEnumerator Ending()
+    {
+        anim.SetBool("Fade", true);
+        yield return new WaitUntil(() => black.color.a == 1);
+        SceneManager.LoadScene(sceneName);
+    }
+}
