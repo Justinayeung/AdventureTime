c99d109546b87566a2bf4e19da01dc3fe93f77de
songy696 <ysong.yjs@gmail.com>
2020-03-08 23:05:17 -0400

    animation for the ending scene

---
+++ AdventureTime/Assets/Scripts/AxDestroy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxDestroy : MonoBehaviour
+{
+    void Start()
+    {
+        if (StaticClass.haveAx == true)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+}
---
+++ AdventureTime/Assets/Scripts/mushroomDestroy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mushroomDestroy : MonoBehaviour
+{
+    void Start()
+    {
+        if (StaticClass.haveMushroom == true)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
